76401877604766d56356ae2367408b71133b8a68
Vinicius Cainelli <vinicius@viniciuscainelli.com>
2015-05-22 13:23:20 +0000

    Created a micro scheduler like Celery
    Description:
    
      * Create a server which listen for a key in redis
      * Create a library (Micro Celery) that supports:
        * Create of a micro celery app
        * Decorate your functions with @app.task
        * Implement .delay() behavior
    
    Test Plan:
    
      * Basic system test:
    
        # Start server
        server$ export PYTHONPATH=$PWD
        server$ python microcelery/server.py -a tests.tasks.app
    
        # Run a bunch of tasks
        client$ export PYTHONPATH=$PWD
        client$ while true; do python tests/client.py; done
    
    Missing (Santa Clauss is seeing you!):
    
      * No unit tests
      * No setup.py (will solve export PYTHONPATH issue)
      * No constraint on the number of processes

---
+++ microcelery/__init__.py
@@ -0,0 +1,1 @@
+from .microcelery import MicroCelery
---
+++ microcelery/server.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+
+import re
+import json
+import time
+import redis
+import argparse
+import importlib
+import multiprocessing
+
+import settings
+
+
+class Server(object):
+    
+    def __init__(self, app):
+        self.rds = redis.Redis()
+        self.app = app
+
+    def _execute_task(self, raw_task):
+        funcname, args, kwargs = json.loads(raw_task)
+        func = self.app._tasks[funcname]
+        proc = multiprocessing.Process(target=func, args=args, kwargs=kwargs)
+        proc.start()
+
+    def _listen_queue(self):
+        for raw_task in self.rds.lrange(settings.QUEUE_KEY, 0, -1):
+            self._execute_task(raw_task)
+            self.rds.lpop(settings.QUEUE_KEY)
+        time.sleep(0.5)
+
+    def run(self):
+        while True:
+            self._listen_queue()
+
+
+def import_app(app_path):
+    module_path, app_name = re.match(r"(.*)\.(.*)", app_path).groups()
+    module = importlib.import_module(module_path)
+    return getattr(module, app_name)
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Microwave, microsoft, microcelery, stuffs like that")
+    parser.add_argument("-A", "--app-path", required=True, help="Application path"
+    )
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = get_args()
+    app = import_app(args.app_path)
+    server = Server(app) 
+    server.run()
+
